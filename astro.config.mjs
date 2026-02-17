a77f07f0be4124ef4934ddf5e96c4beed5339759
RattanaRattana <sengrattana168@gmail.com>
2026-02-17 16:50:32 +0900

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://rattanarattana.github.io',
-  integrations: [mdx(), tailwind()]
+  integrations: [
+    mdx(),
+    tailwind(),
+  ],
 });
